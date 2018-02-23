d6549eab5f7e6024a1a46f2a19bbccc27bcea204
Dustin <dustinknopoff@gmail.com>
2018-02-23 17:08:47 -0500

    works?

--- toThings.py
+++ toThings.py
@@ -1,49 +1,30 @@
 import requests, re
-import webbrowser, appex
+import webbrowser #, appex
 
 # For Testing
-# content = """# Groceries
-# #todo
-# ---
-# ## Costco
-# - Broccoli
-# - Asparagus
-# - Green Beans
-# - Thighs
-# - Breasts
-# - Skirt Steak
-# + Salmon
-# - *Olive Oil*
-
-# ## Star Market
-# + Tomatoes
-# + Garlic
-# + Onions
-# + Mushrooms
-# - Zucchini
-# - Canned Tomatoes
-# - Frozen Veggies
-# - Cauliflower
-# - Fresh Mozzarella 
-# + Shredded Mozzarella 
-# + Prosciutto 
-# + Hot sauce
-# + Prosciutto or pepperoni 
-# + Lemons
-# + Shrimp/Seafood Mix
-# ## Target
-# + Body Wash
-# + Paper Towels
-# + Floss picks
-# + Tin foil
-# + Garbage bags 
-# + Hand soap refill
-# + Wipes"""
+content = """# My App Wishlist
+#wants
+---
++ Pythonista 3 $9.99 [Check it out](https://itunes.apple.com/us/app/pythonista-3/id1085978097?mt=8&uo=4)
++ Fantastical 2 for iPad $15.99 [Check it out](https://itunes.apple.com/us/app/fantastical-2-for-iphone/id718043190?mt=8&uo=4)
+- Things 3 for iPad $19.99 [Check it out](https://itunes.apple.com/us/app/things-3-for-ipad/id904244226?mt=8&uo=4) 
++ iA Writer $4.99 [Check it out](https://itunes.apple.com/us/app/ia-writer/id775737172?mt=8&uo=4)
++ Screens VNC $14.99 [Check it out](https://itunes.apple.com/us/app/screens-vnc/id655890150?mt=8&uo=4)
++ Things 3 for Mac $49.99 [Check it out](https://itunes.apple.com/us/app/things-3/id904280696?mt=12).
+- Affinity Photo for Mac $49.99 [Check it out]( https://itunes.apple.com/us/app/affinity-photo/id824183456?mt=12)
+- Affinity Designer for Mac $49.99 [Check it out](https://itunes.apple.com/us/app/affinity-designer/id824171161?mt=12)
+- Kaleidoscope 2 $19.99 [Check it out](https://itunes.apple.com/us/app/kaleidoscope-2/id1273771160?mt=8&uo=4)
+- Dash $24.99 [Check it out](https://kapeli.onfastspring.com/dash) (50% off for students)
+- Alfred (Powerpack) ￡35 [Check it out](https://www.alfredapp.com/powerpack/) ($50)
++ FileBrowser - Computers+Cloud $5.99 [Check it out](https://itunes.apple.com/us/app/filebrowser-computers-cloud/id364738545?mt=8&uo=4)
+- lire (Full-text RSS) $6.99 [Check it out](https://itunes.apple.com/us/app/lire-full-text-rss/id550441545?mt=8&uo=4)
+- iCab Mobile (Web Browser) $1.99 [Check it out](https://itunes.apple.com/us/app/icab-mobile-web-browser/id308111628?mt=8&uo=4)"""
 def main():
-    content = appex.get_text();
+   # content = appex.get_text();
     if not content:
         print('No input text found. Use this script from the share sheet in an app like Bear.')
         return
+    re.sub(r'[^\x00-\x7F]+', ' ', content)
     headings = re.findall(r'# .*?\n', content)
     tasks = re.findall(r'[+] .*?\n', content)
     finalH = []
