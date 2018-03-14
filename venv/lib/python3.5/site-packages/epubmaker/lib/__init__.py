e4c6f8cc5669457f8c82128e29e0f527a6eecdde
Dustin <dustinknopoff@gmail.com>
2018-03-14 14:46:09 -0400

    cleaning

--- venv/lib/python3.5/site-packages/epubmaker/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-""" This is a package. """
-
-__all__ = ['DublinCore', 'DummyConnectionPool',
-           'GutenbergDatabaseDublinCore', 'GutenbergDatabase',
-           'GutenbergGlobals', 'Logger', 'MediaTypes']
--- venv/lib/python3.5/site-packages/fanfiction/__init__.py
+++
@@ -1,1 +0,0 @@
-from fanfiction.scraper import Scraper
