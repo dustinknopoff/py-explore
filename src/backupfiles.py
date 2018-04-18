39bddc66bdca1724882c30f90c57910853dc176a
Dustin <dustinknopoff@gmail.com>
2018-04-17 23:58:55 -0400

    “done”

---
+++ src/backupfiles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import subprocess
+
+with open('/Users/Dustin/.zshrc', 'r') as f:
+    zsh = f.read()
+    with open('/Users/Dustin/Library/Lima/Documents/Config Files/zshrc.txt', 'w+') as out:
+        out.write(zsh)
+
+brews = subprocess.run(['brew', 'list'], stdout=subprocess.PIPE)
+with open('/Users/Dustin/Library/Lima/Documents/Config Files/brew.txt', 'wb') as out2:
+    out2.write(brews.stdout)
+
+codeext = subprocess.run(['code', '--list-extensions'], stdout=subprocess.PIPE)
+with open('/Users/Dustin/Library/Lima/Documents/Config Files/vscodeextensions.txt', 'wb') as out3:
+    out3.write(codeext.stdout)
