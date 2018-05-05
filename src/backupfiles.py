cd22848d7568cfa32cf039c2ab1364c42ce7bbc1
Dustin <dustinknopoff@gmail.com>
2018-05-05 12:09:22 -0400

    removed lima from backup

--- src/backupfiles.py
+++ src/backupfiles.py
@@ -3,13 +3,13 @@ import subprocess
 
 with open('/Users/Dustin/.zshrc', 'r') as f:
     zsh = f.read()
-    with open('/Users/Dustin/Library/Lima/Documents/Config Files/zshrc.txt', 'w+') as out:
+    with open('/Users/Dustin/Documents/Dropbox/Config Files/zshrc.txt', 'w+') as out:
         out.write(zsh)
 
 brews = subprocess.run(['brew', 'list'], stdout=subprocess.PIPE)
-with open('/Users/Dustin/Library/Lima/Documents/Config Files/brew.txt', 'wb') as out2:
+with open('/Users/Dustin/Documents/Dropbox/Config Files/brew.txt', 'wb') as out2:
     out2.write(brews.stdout)
 
 codeext = subprocess.run(['code', '--list-extensions'], stdout=subprocess.PIPE)
-with open('/Users/Dustin/Library/Lima/Documents/Config Files/vscodeextensions.txt', 'wb') as out3:
+with open('/Users/Dustin/Documents/Dropbox/Config Files/vscodeextensions.txt', 'wb') as out3:
     out3.write(codeext.stdout)
