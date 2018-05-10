c0ced5f64158484c998b5ec44a899351d5d43c7d
Dustin <dustinknopoff@gmail.com>
2018-05-10 11:09:31 -0400

    not clearance at vivo too

--- src/vivo.py
+++ src/vivo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import webbrowser
 
 import requests
@@ -9,23 +10,25 @@ def main():
     """
     Adds reminder to Things.app if there is a pair of shoes under $60 in size 10.
     """
-    results = getcontent()
+    clear = 'https://www.vivobarefoot.com/us/shop/sale#q=size.43~gender.Mens'
+    reg = 'https://www.vivobarefoot.com/us/mens#q=size.43'
+    results = [getcontent(clear), getcontent(reg)]
     flag = False
     for result in results:
-        if int(result['Price'].replace('$', '')) <= 60:
-            webbrowser.open('things:///add?title=Vivo%20Shoes%20On%20Sale!&'
-                            'notes=https://www.vivobarefoot.com/us/shop/sale#q=size.43~gender.Mens&when=Today')
-            flag = True
+        for elem in result:
+            if int(elem['Price'].replace('$', '')) <= 60:
+                webbrowser.open('things:///add?title=Vivo%20Shoes%20On%20Sale!&'
+                                'notes=https://www.vivobarefoot.com/us/shop/sale#q=size.43~gender.Mens&when=Today')
+                flag = True
     if flag is False:
         print('Nothing below $60 in size 10 today')
 
 
-def getcontent():
+def getcontent(site):
     """
     Scrapes VivoBarefoot website for clearance men's shoes
     :return: list of dictionaries containing product names, prices, and links
     """
-    site = 'https://www.vivobarefoot.com/us/shop/sale#q=size.43~gender.Mens'
     r = requests.get(site).content
     soup = BeautifulSoup(r, 'html.parser')
     products = soup.find('ul', attrs={'class': 'products'})
@@ -38,7 +41,10 @@ def getcontent():
             if 'Women' not in names:
                 d['Product'] = names
                 d['Link'] = 'https://www.vivobarefoot.com' + li.a.get('href')
-                price = li.find('span', attrs={'class', 'sale'}).get_text()
+                try:
+                    price = li.find('span', attrs={'class', 'sale'}).get_text()
+                except AttributeError:
+                    price = li.find('span', attrs={'class', 'price'}).get_text()
                 d['Price'] = price
                 results.append(d)
     return results
